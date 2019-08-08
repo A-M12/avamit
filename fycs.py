51e6b5e80e7bc37673fd9cb77b3ac03f560d4c4d
A-M12 <53898798+A-M12@users.noreply.github.com>
2019-08-08 15:44:03 +0530

    Create fycs.py

---
+++ fycs.py
@@ -0,0 +1,16 @@
+cn=input('Enter Name:')
+units=eval(input('Enter no. of units consumed:'))
+dc=50
+if units>500:
+    charge=3000+dc
+elif units>100 and units<=200:
+    charge=500+dc
+elif units>200 and units<=300:
+    charge=650+dc
+elif units>300 and units<=500:
+    charge=800+dc
+else:
+    charge=0+dc
+print('Name=',cn)
+print('Units used=',units)
+print('charges=',charge)
